d237c5d3f6ac68a671b8604034c7875f20d7fbd7
Mariam <74742426+mariamdateshidze@users.noreply.github.com>
2022-04-11 12:12:26 +0400

    add html and js files

---
+++ app.js
@@ -0,0 +1,43 @@
+// 1
+// const currentDay = newDate().getDay();
+// switch(currentDay) {
+//     case 0:
+//     console.log("currentDay==orshabati")
+// }
+
+
+// 2
+
+for (let i = 0; i < 101; i ++) {
+ 	// console.log(i);
+}
+
+
+// 3
+let i = 0;
+while (i < 50) {
+	i++;
+    // console.log(i);
+}
+
+// 4
+let index=0;
+do {
+    index++;
+	// console.log(index);
+} while (index < 150);
+
+
+// 5
+let myArr =[]
+j=0;
+do{
+    j++;
+    myArr.push(j*j)
+}
+while (j<1000);
+// console.log(myArr);
+
+
+
+
