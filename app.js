65561afdf90ad644bfa6dcbc35e6b894e1ca822c
Mariam <74742426+mariamdateshidze@users.noreply.github.com>
2022-04-11 15:42:05 +0400

    add changes

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 // 1
-// const currentDay = newDate().getDay();
-// switch(currentDay) {
-//     case 0:
-//     console.log("currentDay==orshabati")
-// }
+const currentDay = new Date().getDay();
 
+switch (currentDay) {
+    case 0:
+    	console.log("Sunday");
+    	break;
+    case 1:
+    	console.log("Monday");
+    	break;
+    case 2:
+    	console.log("Tuesday");
+        break;
+    case 3:
+    	console.log("Wednesday");
+    	break;
+    case 4:
+    	console.log("Thursday");
+    	break;
+    case 5:
+    	console.log("Friday");
+    	break;
+    case 6:
+    	console.log("Saturday");
+    	break;
+}
 
 // 2
 
@@ -20,6 +39,8 @@ while (i < 50) {
     // console.log(i);
 }
 
+
+
 // 4
 let index=0;
 do {
@@ -28,12 +49,13 @@ do {
 } while (index < 150);
 
 
+
 // 5
-let myArr =[]
-j=0;
+let myArr =[];
+let j=0;
 do{
     j++;
-    myArr.push(j*j)
+    myArr.push(j*j);
 }
 while (j<1000);
 // console.log(myArr);
